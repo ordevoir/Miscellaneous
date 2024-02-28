6d67bab48a1296f03804f387c8664c66da7eb50a
ordevoir <wernadsky@gmail.com>
2024-02-28 17:44:58 +0300

    update

--- plot_tools.py
+++ plot_tools.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+FACECOLOR = "#333"
+FONTCOLOR = "#CCC"
+
 def bgr2rgb(image):
     image_rgb = image.copy()
     for y in range(image_rgb.shape[0]):
@@ -10,19 +13,20 @@ def bgr2rgb(image):
 
 
 def show_image(image: np.ndarray, title=None, size=8, 
-                facecolor="gray", **options):
+                facecolor=FACECOLOR, color=FONTCOLOR, **options):
     plt.figure(figsize=(size, size), facecolor=facecolor)
     plt.axis("off")
-    plt.title(title)
+    plt.title(title, color=color)
     plt.imshow(image, **options)
     plt.show()
 
 
 def show_image_bgr(image: np.ndarray, title=None, size=8, 
-                   facecolor="gray", **options):
-    show_image(bgr2rgb(image), title, size, facecolor, **options)
+                   facecolor=FACECOLOR, color=FONTCOLOR, **options):
+    show_image(bgr2rgb(image), title, size, facecolor, color, **options)
 
-def show_images(images, titles=None, n_cols=2, size=4, facecolor="gray", **options):
+def show_images(images, titles=None, n_cols=2, size=4, 
+                facecolor=FACECOLOR, color=FONTCOLOR, **options):
 
     if titles is not None:
         assert len(titles) == len(images), "images count and titles count must be same"
@@ -32,13 +36,14 @@ def show_images(images, titles=None, n_cols=2, size=4, facecolor="gray", **optio
 
     if img_count == 1:
         title = None if titles is None else titles[0]
-        show_image(images[0], title, size, facecolor, **options)
+        show_image(images[0], title, size, facecolor, color, **options)
         return
 
     n_cols = min(img_count, n_cols)
     n_rows = (img_count - 1) // n_cols + 1
     fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, 
                              figsize=(size*n_cols, size*n_rows*ratio))
+    
     fig.set_facecolor(facecolor)
     
     if n_rows == 1:
@@ -46,7 +51,7 @@ def show_images(images, titles=None, n_cols=2, size=4, facecolor="gray", **optio
             axes[i].axis("off")
             axes[i].imshow(img, **options)
             if titles:
-                axes[i].set_title(titles[i])
+                axes[i].set_title(titles[i], {"color": color})
 
     else:
         for i, img in enumerate(images):
@@ -55,7 +60,8 @@ def show_images(images, titles=None, n_cols=2, size=4, facecolor="gray", **optio
             axes[row, col].axis("off")
             axes[row, col].imshow(img, **options)
             if titles:
-                axes[row, col].set_title(titles[i])
+                axes[row, col].set_title(titles[i], {"color": color})
+
         
         while i < n_rows * n_cols:
             row, col = divmod(i, n_cols)
@@ -65,8 +71,9 @@ def show_images(images, titles=None, n_cols=2, size=4, facecolor="gray", **optio
     plt.tight_layout()
     plt.show()
 
-def show_images_bgr(images, titles=None, n_cols=2, size=4, facecolor="gray", **options):
+def show_images_bgr(images, titles=None, n_cols=2, size=4, 
+                    facecolor=FACECOLOR, color=FONTCOLOR, **options):
     images_rgb = []
     for image in images:
         images_rgb.append(bgr2rgb(image))
-    show_images(images_rgb, titles, n_cols, size, facecolor, **options)
+    show_images(images_rgb, titles, n_cols, size, facecolor, color, **options)
